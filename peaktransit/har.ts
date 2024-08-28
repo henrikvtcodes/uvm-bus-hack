503cf3d96a30ad0cd41916f1ccf5a40053e60e8b
henrikvtcodes <commits@henrikvt.com>
2024-08-28 16:12:15 -0400

    peaktransit api client

---
+++ peaktransit/har.ts
@@ -0,0 +1,127 @@
+export interface HARFile {
+  log: Log;
+}
+
+export interface Log {
+  version: string;
+  creator: Creator;
+  browser: Browser;
+  pages: Page[];
+  entries: Entry[];
+}
+
+export interface Creator {
+  name: string;
+  version: string;
+}
+
+export interface Browser {
+  name: string;
+  version: string;
+}
+
+export interface Page {
+  id: string;
+  pageTimings: PageTimings;
+  startedDateTime: string;
+  title: string;
+}
+
+export interface PageTimings {
+  onContentLoad: number;
+  onLoad: number;
+}
+
+export interface Entry {
+  startedDateTime: string;
+  request: Request;
+  response: Response;
+  cache: Cache;
+  timings: Timings;
+  time: number;
+  pageref: string;
+  _securityState?: string;
+  serverIPAddress?: string;
+  connection?: string;
+}
+
+export interface Request {
+  bodySize: number;
+  method: string;
+  url: string;
+  httpVersion: string;
+  headers: Header[];
+  cookies: any[];
+  queryString: QueryString[];
+  headersSize: number;
+}
+
+export interface Header {
+  name: string;
+  value: string;
+}
+
+export interface QueryString {
+  name: string;
+  value: string;
+}
+
+export interface Response {
+  status: number;
+  statusText: string;
+  httpVersion: string;
+  headers: Header2[];
+  cookies: any[];
+  content: Content;
+  redirectURL: string;
+  headersSize: number;
+  bodySize: number;
+}
+
+export interface Header2 {
+  name: string;
+  value: string;
+}
+
+export interface Content {
+  mimeType?: string;
+  size?: number;
+  text?: string;
+  encoding?: string;
+}
+
+export interface Cache {}
+
+export interface Timings {
+  blocked?: number;
+  dns?: number;
+  connect?: number;
+  ssl?: number;
+  send?: number;
+  wait?: number;
+  receive?: number;
+}
+
+import fs from "fs/promises";
+
+async function getAllUrlsAndComponents() {
+  const harFile = Bun.file("./uvm_peaktransit_24-08-28.har");
+  const harContents = JSON.parse(await harFile.text()) as HARFile;
+
+  const controllers = new Set<string>();
+  const keys = new Set<string>();
+
+  for (const entry of harContents.log.entries) {
+    const endpoint = new URL(entry.request.url);
+
+    const ctrlr = endpoint.searchParams.get("controller");
+
+    if (typeof ctrlr === "string") controllers.add(ctrlr);
+  }
+
+  for (const controller of controllers) {
+    console.log(controller);
+  }
+}
+
+await getAllUrlsAndComponents();
---
+++ peaktransit/index.ts
@@ -0,0 +1,234 @@
+// UVM's Agency ID on PeakTransit
+const AGENCY_ID = 159 as const;
+// This isn't secret but I suspect they change it as a shitty security thing. To validate/update,
+// head to https://uvm.rider.peaktransit.com/ , open network devtools, and look at the `key` url param.
+// Copy it from there if needed.
+const API_URL_KEY_PARAM = "c620b8fe5fdbd6107da8c8381f4345b4" as const;
+
+export const PEAKTRANSIT_UVM_ROOT_URL = new URL(
+  `https://api.peaktransit.com/v5/index.php?app_id=_RIDER&key=${API_URL_KEY_PARAM}&action=list&agencyID=${AGENCY_ID}`,
+);
+
+const PEAKTRANSIT_UVM_SHAPES_URL = new URL(PEAKTRANSIT_UVM_ROOT_URL);
+PEAKTRANSIT_UVM_SHAPES_URL.searchParams.append("controller", "shape2");
+
+export interface ShapeResponse {
+  shape: Shape[];
+  success: boolean;
+}
+
+export interface Shape {
+  shapeID: number;
+  agencyID: number;
+  shapeName: string;
+  routeID: number;
+  description: string;
+  points: string;
+  source: string;
+  updated: number;
+  disabled: boolean;
+  directions: string;
+  dynamicTotalShapeTime: number;
+}
+
+const PEAKTRANSIT_UVM_ETA_URL = new URL(PEAKTRANSIT_UVM_ROOT_URL);
+PEAKTRANSIT_UVM_ETA_URL.searchParams.append("controller", "eta");
+
+export async function getETAs() {
+  return (await (
+    await fetch(PEAKTRANSIT_UVM_ROUTESTOP_URL)
+  ).json()) as ETARequest;
+}
+
+export interface ETARequest {
+  stop: StopETA[];
+  success: boolean;
+}
+
+export interface StopETA {
+  stopID: number;
+  routeID: number;
+  ETA1: number;
+  ETA2: number;
+}
+
+const PEAKTRANSIT_UVM_STOPS_URL = new URL(PEAKTRANSIT_UVM_ROOT_URL);
+PEAKTRANSIT_UVM_STOPS_URL.searchParams.append("controller", "stop2");
+
+export async function getStops() {
+  return (await (
+    await fetch(PEAKTRANSIT_UVM_STOPS_URL)
+  ).json()) as StopsRequest;
+}
+
+export interface StopsRequest {
+  stop: Stop[];
+  success: boolean;
+}
+
+export interface Stop {
+  stopID: number;
+  agencyID: number;
+  shortName: any;
+  longName: string;
+  lat: number;
+  lng: number;
+  textID: string;
+  updated: number;
+  disabled: boolean;
+  stopCode: string;
+  aliases: string;
+  stopCodeAlias: string;
+  closed: boolean;
+  sheltered: boolean;
+  source: any;
+  hidden: boolean;
+  depot: boolean;
+}
+
+const PEAKTRANSIT_UVM_VEHICLES_URL = new URL(PEAKTRANSIT_UVM_ROOT_URL);
+PEAKTRANSIT_UVM_VEHICLES_URL.searchParams.append("controller", "vehicles2");
+
+export async function getVehicles() {
+  return (await (
+    await fetch(PEAKTRANSIT_UVM_VEHICLES_URL)
+  ).json()) as VehiclesRequest;
+}
+
+export interface VehiclesRequest {
+  vehicles: Vehicle[];
+  success: boolean;
+}
+
+export interface Vehicle {
+  vehicleID: number;
+  agencyID: number;
+  routeID: number;
+  driverID: number;
+  tripID: number;
+  shapeID: number;
+  vehicleName: string;
+  vehicleType: string;
+  numPax: number;
+  lat: number;
+  lng: number;
+  positionUpdated: number;
+  speed: number;
+  course: number;
+  canAcceptDispatchRequests: boolean;
+  checklistID: number;
+  supervisor: boolean;
+  trail: string;
+  updated: number;
+  disabled: boolean;
+  hidden: boolean;
+  externalVehicleID: string;
+  externalRouteID: string;
+  oos: string;
+  source?: string;
+  icon: number;
+  atLink: number;
+  linkLat: number;
+  linkLng: number;
+  linkUpdated: number;
+  linkChanged: number;
+  linkBearing: number;
+  minsLate: number;
+  minsLateUpdated: number;
+  onAssignment: number;
+  mpg: number;
+  fuelType: string;
+  HasAPC: boolean;
+  showCounts: boolean;
+  APCPercentage: number;
+  APCCapacity: number;
+  APCCount: number;
+  APCChanged: number;
+  batteryLevel: number;
+  batteryState: number;
+  nextStopID: number;
+  nextStopETA: number;
+  nextStopUnscheduled: boolean;
+  cat: number;
+  alert: boolean;
+  routeUpdated: number;
+  altitude: number;
+  verticalSpeed: number;
+  headway: number;
+  holdAtStopID: number;
+  holdTime: number;
+  waitTime: number;
+  DSUpdated: number;
+  desiredHeadway: number;
+  mdtUpdate: number;
+  mdtVersion: string;
+  mdtSerial: any;
+  routeSource: string;
+  vehicleState: number;
+  positionSource?: string;
+}
+
+const PEAKTRANSIT_UVM_ROUTES_URL = new URL(PEAKTRANSIT_UVM_ROOT_URL);
+PEAKTRANSIT_UVM_ROUTES_URL.searchParams.append("controller", "route2");
+
+export async function getRoutes() {
+  return (await (
+    await fetch(PEAKTRANSIT_UVM_ROUTES_URL)
+  ).json()) as RoutesRequest;
+}
+
+export interface RoutesRequest {
+  routes: Route[];
+  success: boolean;
+}
+
+export interface Route {
+  routeID: number;
+  agencyID: number;
+  shortName: string;
+  longName: string;
+  routeType: number;
+  sequence: number;
+  description: string;
+  color: string;
+  textColor: string;
+  onDemand: boolean;
+  calcETA: boolean;
+  hidden: boolean;
+  updated: number;
+  disabled: boolean;
+  scheduleURL: string;
+  scheduleURLType: string;
+  shapeID: number;
+  schedAdh: boolean;
+  aliases: string;
+  source: any;
+  LastETACalc: number;
+  routeCode: string;
+  special_service: boolean;
+}
+
+const PEAKTRANSIT_UVM_ROUTESTOP_URL = new URL(PEAKTRANSIT_UVM_ROOT_URL);
+PEAKTRANSIT_UVM_ROUTESTOP_URL.searchParams.append("controller", "routestop2");
+
+export async function getRouteStops() {
+  return (await (
+    await fetch(PEAKTRANSIT_UVM_ROUTESTOP_URL)
+  ).json()) as RouteStopsRequest;
+}
+
+export interface RouteStopsRequest {
+  routeStops: RouteStop[];
+  success: boolean;
+}
+
+export interface RouteStop {
+  routeStopID: number;
+  agencyID: number;
+  routeID: number;
+  stopID: number;
+  link: number;
+  sequence: any;
+  updated: number;
+  disabled: boolean;
+}
